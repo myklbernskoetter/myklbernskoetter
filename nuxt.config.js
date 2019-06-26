8295a089205dd4e76fcb07d1d2f8014578b83efd
Mykl Bernskoetter <mykl@wearerivet.com>
2019-06-26 12:32:59 -0500

    Nuxt update removed eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,18 +28,5 @@ module.exports = {
   */
   build: {
     extractCSS: true,
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
   }
 }
